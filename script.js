8ac83e07e0eeeda891dc9ca706f11b538242351c
Suyash Mishra <141909257+Suyash878@users.noreply.github.com>
2024-02-03 10:36:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+const n = 10;
+const array = [];
+
+init();
+
+function init()
+{
+    for(let i = 0 ; i < 10 ; i++)
+    {
+    array[i] = Math.random();
+    }
+    showBars();
+}
+
+function BubbleSort(array)
+{
+    const swaps = [];
+
+do{
+    var swapped = false;
+
+    for(let i = 0 ; i < array.length ; i++)
+    {
+        if(array[i-1] > array[i])
+        {
+            swapped = true;
+            swaps.push([i-1,i]);
+            [array[i],array[i-1]] = [array[i-1],array[i]];
+            }
+    }
+}while(swapped)
+return swaps;
+}
+
+function play()
+{   
+    const copy = [...array];
+    const swaps = BubbleSort(copy);
+    animate(swaps);
+}
+
+function animate(swaps)
+{
+    if(swaps.length==0)
+    {
+        return;
+    }
+
+    const [i,j] = swaps.shift();
+    [array[i],array[j]] = [array[j],array[i]];
+    showBars([i,j]);
+    setTimeout(function(){animate(swaps);},50);
+}
+
+function showBars()
+{
+container.innerHTML = ""; //clears the present values so that the next ones can appear. otherwise they keep appearing next to each other.
+for(let i = 0 ; i < array.length ; i++)
+{
+    const bar = document.createElement("div");
+    bar.style.height = array[i]*100+"%";
+    bar.style.width = "10px";
+    bar.style.backgroundColor = "black";
+    bar.classList.add("bar");
+    container.appendChild(bar);
+
+}
+}
